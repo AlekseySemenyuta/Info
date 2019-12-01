fbfb2ad38fd051ccb4929853582324191c82187d
AlekseySemenyuta <43886219+AlekseySemenyuta@users.noreply.github.com>
2019-12-01 20:57:12 +0300

    .

---
+++ 25_11_19/g.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "0: 1, 2, 3, 4, 5" << endl;
+	cout << "1: 0, 2, 3, 4, 5" << endl;
+	cout << "2: 0, 1, 3, 4, 5" << endl;
+	cout << "3: 0, 1, 2, 5, 6" << endl;
+	cout << "4: 0, 1, 2, 7, 8" << endl;
+	cout << "5: 0, 1, 2, 3, 9" << endl;
+	cout << "6: 3, 7, 8, 10, 11" << endl;
+	cout << "7: 4, 6, 9, 10, 11" << endl;
+	cout << "8: 4, 6, 9, 10, 11" << endl;
+	cout << "9: 5, 7, 8, 9, 11" << endl;
+	cout << "10: 6, 7, 8, 9, 11" << endl;
+	cout << "11: 6, 7, 8, 9, 10" << endl;
+	return 0;
+}
